fdb79aa2356cebb0aa6e81aeac8bd22d2c1ce44c
Anecha Kuekharem <Anachitheu.k@gmail.com>
2024-07-22 16:32:39 +0000

    build: eslint

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,16 +5,18 @@ import { MESSAGE } from '@/common/constants';
 import type { CreateUserValues, FindUserOptions, UpdateUserValues } from './user.interface';
 
 // TODO: Filters data
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const find = async (query?: FindUserOptions) => {
   return User.findAll();
 };
 
 // TODO: Filters data
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const count = async (query?: FindUserOptions) => {
-  return User.findAll();
+  return User.count();
 };
 
-const findById = async (id: string) => {
+const findById = async (id: string | number) => {
   const entity = await User.findOne({ where: { id } });
   if (!entity) {
     throw createHttpError(httpStatus.NOT_FOUND, MESSAGE.RESOURCE_NOT_FOUND);
@@ -28,13 +30,13 @@ const create = async (data: CreateUserValues) => {
   return entity;
 };
 
-const update = async (id: string, data: UpdateUserValues) => {
+const update = async (id: string | number, data: UpdateUserValues) => {
   const entity = await findById(id);
   const updated = await entity.update(data);
   return updated;
 };
 
-const destroy = async (id: string) => {
+const destroy = async (id: string | number) => {
   const entity = await findById(id);
   return entity.destroy();
 };
